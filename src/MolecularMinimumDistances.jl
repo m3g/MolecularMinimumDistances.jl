e7ea2cf5acecd3278917e936609c9c53a299f401
Leandro Martinez <leandro@iqm.unicamp.br>
2022-03-07 13:28:01 -0300

    first code commit

--- src/MolecularMinimumDistances.jl
+++ src/MolecularMinimumDistances.jl
@@ -1,5 +1,59 @@
 module MolecularMinimumDistances
 
-# Write your package code here.
+using CellListMap
 
+export MinimumDistance
+export minimum_distances, minimum_distances!
+
+struct MinimumDistance{T}
+    i::Int
+    d::T
 end
+
+struct CellListParameters{Box,CellList,AuxThreaded}
+    box::Box
+    cl::CellList
+    aux::AuxThreaded
+end
+
+function update_list!(
+    i, j, d2,
+    x_molecule_of_atom::AbstractVector{<:Integer},
+    y_molecule_of_atom::AbstractVector{<:Integer},
+    lists::NamedTuple{(:x_list, :y_list), <:Tuple}
+)
+    (;x_list, y_list) = lists
+    d = sqrt(d2)
+    imol = x_molecule_of_atom[i]
+    if d < list[imol].d
+        x_list[imol] = MinimumDistance(j,d)
+    end
+    jmol = y_molecule_of_atom[j]
+    if d < list[jmol].d
+        y_list[imol] = MinimumDistance(i,d)
+    end
+    return lists
+end
+
+function minimum_distances!(
+    x, y,
+    x_molecule_of_atom::AbstractVector{<:Integer},
+    y_molecule_of_atom::AbstractVector{<:Integer},
+    list_x::AbstractVector{<:MinimumDistance},
+    list_y::AbstractVector{<:MinimumDistance},
+    cell_list_parameters::CellListMapParameters,
+)
+    lists = (x_list = x_list, y_list = y_list)
+    (; box, cl, aux = cell_list_parameters)
+    map_pairwise!(
+        (x, y, i, j,d2, lists) -> update_list!(i, j, d2, x_molecule_of_atom, y_molecule_of_atom, lists),
+        lists, box, cl, aux
+    )
+    return list
+end
+
+
+
+
+
+end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,25 @@
 using MolecularMinimumDistances
 using Test
 
+using StaticArrays
+using CellListMap
+
 @testset "MolecularMinimumDistances.jl" begin
-    # Write your tests here.
+
+    x = [ rand(SVector{3,Float64}) for i in 1:30 ]
+    y = [ rand(SVector{3,Float64}) for i in 1:50 ]
+    x_molecules = [ j for j in 1:10 for i in 1:3 ]
+    y_molecules = [ j for j in 1:10 for i in 1:5 ]
+
+    box = Box([1,1,1],0.1)
+    cl = CellList(x,y,box)
+    aux = AuxThreaded(cl)
+    cell_list_parameters = MolecularMinimumDistances.CellListParameters(bo,cl,aux)
+
+
+
+
+
+
+
 end
