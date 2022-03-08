47cd8c51147102a02363170a82f6470485e25752
Leandro Martinez <leandro@iqm.unicamp.br>
2022-03-08 11:15:55 -0300

    test development

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,20 +1,22 @@
 using MolecularMinimumDistances
 using Test
 
+import PDBTools
 using StaticArrays
 using CellListMap
 
 @testset "MolecularMinimumDistances.jl" begin
 
-    x = [ rand(SVector{3,Float64}) for i in 1:30 ]
-    y = [ rand(SVector{3,Float64}) for i in 1:50 ]
-    x_molecules = [ j for j in 1:10 for i in 1:3 ]
-    y_molecules = [ j for j in 1:10 for i in 1:5 ]
+
+    atoms = PDBTools.readPDB("./simple_test.pdb")
+    x = PDBTools.select(atoms,"resname HOH") 
+    y = PDBTools.select(atoms,"not resname HOH")
+
 
     box = Box([1,1,1],0.1)
     cl = CellList(x,y,box)
     aux = AuxThreaded(cl)
-    cell_list_parameters = MolecularMinimumDistances.CellListParameters(bo,cl,aux)
+    cell_list_parameters = MolecularMinimumDistances.CellListParameters(box,cl,aux)
 
 
 
