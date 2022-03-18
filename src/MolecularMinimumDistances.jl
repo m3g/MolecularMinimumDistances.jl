fc0dca936972e0675b5a013067c22c021cdbd15d
Leandro Martinez <lmartine@unicamp.br>
2022-03-18 11:11:51 -0300

    removed need for cl_data

--- src/MolecularMinimumDistances.jl
+++ src/MolecularMinimumDistances.jl
@@ -11,12 +11,6 @@ struct MinimumDistance{T}
     d::T
 end
 
-mutable struct CellListMapData{Box,CellList,AuxThreaded}
-    box::Box
-    cl::CellList
-    aux::AuxThreaded
-end
-
 function update_list!(
     i, j, d2,
     molecule_of_i::AbstractVector{<:Integer},
@@ -63,29 +57,20 @@ function reduce_list_pair!(lists, lists_threaded)
 end
 
 function minimum_distances!(
-    x, y,
     molecule_of_i::AbstractVector{<:Integer},
     molecule_of_j::AbstractVector{<:Integer},
     x_list::AbstractVector{<:MinimumDistance},
     y_list::AbstractVector{<:MinimumDistance},
-    cell_list_data::CellListMapData;
+    box, cl, aux;
     parallel = true
 )
-    @unpack box, cl, aux = cell_list_data
     lists = (x_list = x_list, y_list = y_list)
-    # Update the cell lists with the new coordinates and box
-    #cl = UpdateCellList!(x, y, box, cl, aux)
     map_pairwise!(
         (x, y, i, j, d2, lists) -> update_list!(i, j, d2, molecule_of_i, molecule_of_j, lists),
         lists, box, cl; 
         parallel=parallel,
         reduce = reduce_list_pair!,
     )
-    # Update the data container, for possible reuse
-    cell_list_data.cl = cl
-    cell_list_data.aux = aux
-
-    # Return updated lists 
     return x_list, y_list
 end
 
@@ -96,12 +81,11 @@ function minimum_distances(
     parallel ? nbatches = (0,0) : nbatches=(1,1)
     cl = CellList(x,y,box,nbatches=nbatches)
     aux = CellListMap.AuxThreaded(cl)
-    cell_list_data = CellListMapData(box,cl,aux)
     molecule_of_i = molecule_indices(x, n_atoms_per_molecule_x)
     molecule_of_j = molecule_indices(y, n_atoms_per_molecule_y)
     x_list = fill(MinimumDistance(-1,typemax(eltype(x[begin]))),length(x)÷n_atoms_per_molecule_x)
     y_list = fill(MinimumDistance(-1,typemax(eltype(y[begin]))),length(y)÷n_atoms_per_molecule_y)
-    minimum_distances!(x, y, molecule_of_i, molecule_of_j, x_list, y_list, cell_list_data; parallel=parallel)
+    minimum_distances!(molecule_of_i, molecule_of_j, x_list, y_list, box, cl, aux; parallel=parallel)
     return x_list, y_list
 end
 
