28038de008463dfad411e2015cd109c7b0f51eb5
Leandro Martinez <lmartine@unicamp.br>
2022-03-22 09:47:24 -0300

    added initial doc files

---
+++ docs/make.jl
@@ -0,0 +1,21 @@
+import Pkg
+Pkg.add("Documenter")
+using Documenter
+using MolecularMinimumDistances
+push!(LOAD_PATH, "../src/")
+makedocs(
+    modules = [MolecularMinimumDistances],
+    sitename = "MolecularMinimumDistances.jl",
+    pages = [
+        "Home" => "index.md",
+        "Basic use" => "basic.md",
+        "Advanced use" => "advanced.md",
+        "Reference" => "reference.md",
+    ],
+)
+deploydocs(
+    repo = "github.com/m3g/MolecularMinimumDistances.jl.git",
+    target = "build",
+    branch = "gh-pages",
+    versions = ["stable" => "v^", "v#.#"],
+)
---
+++ docs/src/reference.md
@@ -0,0 +1,8 @@
+# Citation
+
+If this package was useful, please cite the article describing the main
+algorithms on which it is based:
+
+L. Martínez (2022) CellListMap.jl: Efficient and customizable cell list implementation for calculation of pairwise particle properties within a cutoff.
+[https://doi.org/10.48550/arXiv.2202.06427](https://doi.org/10.48550/arXiv.2202.06427)
+
