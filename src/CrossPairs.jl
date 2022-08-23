80fafcf1c667a5dba4470d54d38deba67812bb0b
Leandro Martinez <leandro@iqm.unicamp.br>
2022-08-22 21:52:12 -0300

    comment changes

--- src/CrossPairs.jl
+++ src/CrossPairs.jl
@@ -56,25 +56,26 @@ atoms and are continously stored in the array of positions).
 julia> using MolecularMinimumDistances, StaticArrays
 
 julia> sys = CrossPairs(
-           xpositions=rand(SVector{3,Float64},100), # "solvent" (set of molecules)
-           ypositions=rand(SVector{3,Float64},10^5), # "solute" (target structure)
+           xpositions=rand(SVector{3,Float64},10^5), # "solvent" (set of molecules)
+           ypositions=rand(SVector{3,Float64},1000), # "solute" (target structure)
            cutoff=0.1,
            unitcell=[1,1,1],
            xn_atoms_per_molecule=5 # of the "solvent"
        )
 CrossPairs system with:
 
-Number of atoms: 100000
+Number of atoms of set: 100000
+Number of atoms of target structure: 1000
 Cutoff: 0.1
 unitcell: [1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0]
-Number of molecules: 20000
+Number of molecules in set: 20000
 
 julia> minimum_distances!(sys)
 20000-element Vector{MinimumDistance{Float64}}:
- MinimumDistance{Float64}(true, 4, 24930, 0.008039482961077074)
- MinimumDistance{Float64}(true, 6, 74055, 0.0049818659155905255)
+ MinimumDistance{Float64}(true, 1, 859, 0.037219109441123784)
+ MinimumDistance{Float64}(true, 10, 117, 0.042183794688796634)
  ⋮
- MinimumDistance{Float64}(true, 99999, 75403, 0.0025051670801269433)
+ MinimumDistance{Float64}(true, 99996, 168, 0.014269620784984633)
 ```
 
 """
